da627613473edb46da54dea37a95b547391c6c68
Ron Weiss <ronweiss@gmail.com>
2009-11-21 18:50:30 -0500

    added matfile_to_barfeats function
    resample is broken with scikits.samplerate...

--- PythonSrc/feats_utils.py
+++ PythonSrc/feats_utils.py
@@ -31,24 +31,40 @@ except:
 # BIG HACK in the case we don't get the exactnew size,
 # we add or remove a delta in the ratio
 ##############################################################
-def resample(data, newsize) :
+def resample(data, newsize):
+    #rs = samplerate.resample
+    rs = lambda x,y: samplerate.resample(x, y, type='sinc_fastest')
+
     delta = .0001
     res = N.zeros([N.shape(data)[0],newsize])
     for l in range(N.shape(data)[0]):
-        newdata  = samplerate.resample(data[l,:],
-                                       newsize * 1. / N.shape(data)[1]);
+        newdata  = rs(data[l,:], newsize * 1. / N.shape(data)[1]);
         if N.asarray(newdata).shape[0] > newsize :
-            newdata  = samplerate.resample(data[l,:],
-                                       newsize * 1. / N.shape(data)[1] - delta);
+            newdata  = rs(data[l,:], newsize * 1. / N.shape(data)[1] - delta);
         if N.asarray(newdata).shape[0] > newsize :
-            newdata  = samplerate.resample(data[l,:],
-                                       newsize * 1. / N.shape(data)[1] + delta);
+            newdata  = rs(data[l,:], newsize * 1. / N.shape(data)[1] + delta);
         if N.asarray(newdata).shape[0] != newsize :
             print 'resample, bad new size!!!'
             continue
         res[l,:] = newdata.T
     return res
 
+
+def matfile_to_barfeats(matfile, newsize=16):
+    """"""
+    mat = read_matfile(matfile)
+    chroma = mat['btchroma']
+    bars = mat['barbts'][:,0]
+
+    barfeats = N.empty((chroma.shape[0] * newsize, len(bars)))
+    for n in xrange(len(bars)):
+        try:
+            end = bars[n+1]
+        except IndexError:
+            end = len(chroma)
+        barfeats[:,n] = resample(chroma[:,bars[n]:end], newsize).flatten()
+
+    return barfeats
 
 
 ##############################################################
