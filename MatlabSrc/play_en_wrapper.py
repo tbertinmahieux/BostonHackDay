afdeaf7c18869431ba9b245ae7d174b4e163513e
tb2332 <tb2332@columbia.edu>
2009-11-22 14:42:48 -0500

    works, plays music

--- MatlabSrc/play_en_wrapper.py
+++ MatlabSrc/play_en_wrapper.py
@@ -21,22 +21,23 @@ import lsh
 # read from a process
 # originally written by Douglas Eck: douglas.eck@umontreal.ca
 #########################################################################
-def command_with_output(cmd):
-    if not type(cmd) == unicode :
-        cmd = unicode(cmd,'utf-8')
-    # hack just for Darwin, those lucky people
-    if sys.platform=='darwin' :
-        cmd = unicodedata.normalize('NFC',cmd)
-    child = os.popen(cmd.encode('utf-8'))
-    data = child.read()
-    err = child.close()
-    return data
+#def command_with_output(cmd):
+#    if not type(cmd) == unicode :
+#        cmd = unicode(cmd,'utf-8')
+#    # hack just for Darwin, those lucky people
+#    if sys.platform=='darwin' :
+#        cmd = unicodedata.normalize('NFC',cmd)
+#    child = os.popen(cmd.encode('utf-8'))
+#    data = child.read()
+#    err = child.close()
+#    return data
 
 
 
 
-def play_en(enid,beat,matdir) :
-    """Try to call play_en matlab code from python."""
+def play_en(enid,matdir,starttime=0,endtime=30) :
+    """Try to call play_en matlab code from python.
+    Start time and stop time in seconds."""
 
     tmpFileIn = 'dummy_playenwrapperpy_infile.mat'
     tmpFileOut = 'dummy_playenwrapperpy_outfile.mat'
@@ -62,6 +63,11 @@ def play_en(enid,beat,matdir) :
 
     # read file
     mfile = scipy.io.loadmat(tmpFileOut)
+
+    # play file
+    print 'playing wavfile...'
+    cmd = 'aplay -q dummy_wavfile.wav'
+    result = lsh.command_with_output(cmd)
 
     return mfile['signal']
 
