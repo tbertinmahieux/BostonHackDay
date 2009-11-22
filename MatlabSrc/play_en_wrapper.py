a1530c7cc578bf953bf965eb22127b99632fede2
tb2332 <tb2332@columbia.edu>
2009-11-22 12:23:19 -0500

    moving stuff

---
+++ MatlabSrc/play_en_wrapper.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env
+"""
+Wrapper around the matlab command play_en from Dan
+needs to have the matlab files + matlab installed!
+"""
+
+import os
+import sys
+import string
+import numpy as N
+
+
+
+
+
+def play_en(M,dur,sr,matFilesDir) :
+    """Try to call play_en matlab code from python."""
+
+
+
