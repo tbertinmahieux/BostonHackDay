fdb4ea8c39a9b8efbf6d869e1c46a60854f800aa
tb2332 <tb2332@columbia.edu>
2010-02-23 13:35:40 -0500

    demos now has a util function that gather the dataset we always use

--- PythonSrc/demos.py
+++ PythonSrc/demos.py
@@ -3,6 +3,35 @@ More of a demo than a useful code
 """
 
 
+def get_data_maxener_16_true_false_bars2():
+    """
+    Util function for something we do all the time
+    Remove the empty patterns
+    """
+    import data_iterator
+    import feats_utils as FU
+    import numpy as np
+    import time
+    # start time
+    tstart = time.time()
+    # get maltab files
+    allfiles = FU.get_all_matfiles('.')
+    # create and set iterator
+    data_iter = data_iterator.DataIterator()
+    data_iter.setMatfiles(allfiles) # set matfiles
+    data_iter.useBars(2)            # a pattern spans two bars
+    data_iter.stopAfterOnePass(True)# stop after one full iteration
+    # get features
+    featsNorm = [FU.normalize_pattern_maxenergy(p,16,True,False).flatten() for p in data_iter]
+    # make it an array
+    featsNorm = np.array(featsNorm)
+    # remove empyt patterns
+    res = [np.sum(r) > 0 for r in featsNorm]
+    res2 = np.where(res)
+    featsNorm = featsNorm[res2]
+    # time?
+    print 'all patterns acquired and normalized in ' + str(time.time()-tstart) + 'seconds'
+    print 'featsNorm.shape = ',featsNorm.shape
 
 
 def get_all_barfeats():
