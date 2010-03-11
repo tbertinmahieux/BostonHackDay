1b1a79c4d5fdac756515547447da34d00d30c8b5
Ron Weiss <ronw@mirfiles.smusic.nyu.edu>
2010-03-11 09:28:49 -0500

    added newline at end of experiment report

--- PythonSrc/for_ron.py
+++ PythonSrc/for_ron.py
@@ -82,7 +82,7 @@ def do_experiment(experiment_dir,beats,bars,nCodes,nSamples=0,useFirsts=False,se
     if useFirsts:
         report.append(['we use firsts %s samples' % nCodes])
     report.extend(['np.average(avg_dists): %s' % np.average(avg_dists),
-                   '************************************************'])
+                   '************************************************', ''])
     reportstr = '\n'.join(report)
     print reportstr
     f = open(os.path.join(experiment_dir, 'report.txt'), 'w')
