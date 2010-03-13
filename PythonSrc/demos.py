3040b1ed08e413c4677bf46addbbf1467a653f41
tb2332 <tb2332@columbia.edu>
2010-03-13 15:47:42 -0500

    added a function to merge codes from codebook, but... does not work well

--- PythonSrc/demos.py
+++ PythonSrc/demos.py
@@ -128,11 +128,65 @@ def encode_one_song(filename,codebook,pSize=8,keyInv=True,
 
 
 def get_codeword_histogram(codewords, ncodewords):
+    """
+    Needs documentation, what is codeworks? best_code_per_pattern....
+    """
     hist = np.zeros(ncodewords)
     for cw in codewords:
         hist[cw] += 1
     return hist
 
+
+
+def merge_codebook(codebook,nGoal,freqs = []):
+    """
+    merge the codebook in an iterative and greedy way.
+    Algo:
+      - finds closest pair of codes
+      - merge them, using freqs if available
+      - repeat until desired number of codes (nGoal)
+    Returns smaller codebook, #codes=nGoal
+    Also returns frequencies of the new codebook
+    Code not optimized!!!!!! close to n^3 operations
+    """
+    import numpy as np
+    import VQutils as VQU
+    import copy
+    # set freqs, sanity checks
+    if freqs == []:
+        freqs = np.ones(codebook.shape[0])
+    freqs = np.array(freqs)
+    assert(freqs.size == codebook.shape[0])
+    assert(nGoal < codebook.shape[0])
+    assert(nGoal > 0)
+    # let's go!
+    cb = copy.deepcopy(codebook)
+    for k in range(codebook.shape[0] - nGoal):
+        # compute dists for all pairs
+        dists = np.zeros([cb.shape[0],cb.shape[0]])
+        for l in range(dists.shape[0]):
+            dists[l,l] = np.inf
+            for c in range(l+1,dists.shape[1]):
+                dists[l,c] = VQU.euclidean_dist(cb[l],cb[c])
+                dists[c,l] = np.inf
+        # find closest pair
+        pos = np.where(dists==dists.min())
+        code1 = pos[0][0]
+        code2 = pos[1][0]
+        print 'iter',k,' min distance=',dists.min(),' codes=',code1,',',code2
+        assert(code1 < code2)#code1 should be smaller from how we filled dists
+        # merge
+        #cb[code1,:] = np.mean([cb[code1,:]*freqs[code1],cb[code2,:]*freqs[code2]],axis=0) * 1. / (freqs[code1] + freqs[code2])
+        cb[code1,:] = np.mean([cb[code1,:],cb[code2,:]],axis=0)
+        freqs[code1] += freqs[code2]
+        # remove
+        if code2 + 1 < cb.shape[0]:
+            cb[code2,:] = cb[-1,:]
+            freqs[code2] = freqs[-1]
+        cb = cb[:-1]
+        freqs = freqs[:-1]
+    # done
+    return cb, freqs
 
 def get_all_barfeats():
     """
